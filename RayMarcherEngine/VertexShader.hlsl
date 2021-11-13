db70e8c4951994bdcc944ff4dc9783411dfb4bae
Anthony Sturdy <anthonysturdy123@gmail.com>
2021-11-13 23:14:46 +0000

    Stripping engine features - Render quad to screen

--- RayMarcherEngine/VertexShader.hlsl
+++ RayMarcherEngine/VertexShader.hlsl
@@ -15,30 +15,14 @@ struct VS_INPUT {
 
 struct PS_INPUT {
     float4 Pos : SV_POSITION;
-    float4 worldPos : POSITION;
-    float3 Norm : NORMAL;
     float2 Tex : TEXCOORD0;
-    float3 Tan : TANGENT;
-    float3 Binorm : BINORMAL;
 };
 
 PS_INPUT VS(VS_INPUT input) {
     PS_INPUT output = (PS_INPUT)0;
-    output.Pos = mul(input.Pos, World);
-    output.worldPos = output.Pos;
-    output.Pos = mul(output.Pos, View);
-    output.Pos = mul(output.Pos, Projection);
     
+    output.Pos = input.Pos;
     output.Tex = input.Tex;
-    
-    output.Norm = mul(input.Norm, (float3x3)World);
-    output.Norm = normalize(output.Norm);
-    
-    output.Tan = mul(input.Tan, (float3x3)World);
-    output.Tan = normalize(output.Tan);
-    
-    output.Binorm = mul(input.Binorm, (float3x3)World);
-    output.Binorm = normalize(output.Binorm);
 
     return output;
 }
